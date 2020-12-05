23f5cc86b65a1b7b1ff3e5199c3aee4e2fcdaaee
jh <jasonhassell228@gmail.com>
2020-12-05 17:41:21 +1000

    Day 5

--- Day5/prob1.py
+++ Day5/prob1.py
@@ -1,71 +1,93 @@
 f = open("input", "r")
-current = []
-valid = 0
+maxID = 0
+rows = []
+columns = []
 for line in f.readlines():
-    # print(line)
-    if line == "\n":
-        print("new passport for", current)
-        if not ('byr' not in current or 'iyr' not in current or 'eyr' not in current or 'hgt' not in current or 'hcl' not in current or 'ecl' not in current or 'pid' not in current):
-            valid += 1
-
-        current = []
-    print("currently processing", line)
-    for i in line.split():
-        t = i[:3]
-        if t == 'byr':
-            if int(i[4:]) < 1920 or int(i[4:]) > 2002:
-                continue
-            print("keep byr")
-        if t == 'iyr':
-            if int(i[4:]) < 2010 or int(i[4:]) > 2020:
-                continue
-            print("keep iyr")
-        if t == 'eyr':
-            if int(i[4:]) < 2020 or int(i[4:]) > 2030:
-                continue
-            print("keep eyr")
-        if t == 'hgt':
-            if i[4:-2] == '':
-                print("shits fucked")
-                continue
-            if i[-2:] == 'cm':
-                if int(i[4:-2]) < 150 or int(i[4:-2]) > 193:
-                    continue
-            elif i[-2:] == 'in':
-                if int(i[4:-2]) < 59 or int(i[4:-2]) > 76:
-                    continue
-            print("keep hgt")
-        if t == 'hcl':
-            count = 0
-            bad = 0
-            if i[4:][0] != '#':
-                print("bad hcl, no hash")
-                continue
-            for d in i[4:][1:]:
-                print("d", d)
-                if d not in {'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}:
-                    print("bad hcl, bad chars", d)
-                    bad = 1
-                count += 1
-            if count != 6 or bad > 0:
-                print("bad hcl, bad count/chars")
-                continue
-            print("keep hcl")
-        if t == 'ecl':
-            if len(i[4:]) != 3:
-                print("ecl bad:", i[4:])
-            if i[4:] not in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}:
-                continue
-            print("keep ecl")
-        if t == 'pid':
-            if len(str(i[4:])) != 9:
-                continue
-            for i in i[4:]:
-                if i not in {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}:
-                    continue
-            print("keep pid")
-        # print("splitting:", t, i[4:])
-        current.append(t)
+    row = 0
+    column = 0
+    start = 127
+    first = 0
+    last = start
+    # for i in line.split():
+        # i is current line
+    print("current line", line[:-1], "splitting", line[:7], line[-4:-1], end=' | ')
+    for c in line[:7]:
+        print(c, end='')
+        if (c == 'F'):
+            last = (last - first) // 2 + first
+        elif (c == 'B'):
+            # print("bs first", first, "last", last)
+            first = ((last + 1) - first) // 2 + first
+            # print("new first", first)
+        # print("first:", first, "last:", last)
+    print()
+    row = first
+    # print("row", row)
+    # print("trying to add row", row)
     
-print(valid)
-    # print(line.split()[0])
+    first = 0
+    last = 7
+    for c in line[-4:-1]:
+        if (c == 'L'):
+            last = (last - first) // 2 + first
+        elif (c == 'R'):
+            first = ((last + 1) - first) // 2 + first
+        # print("first:", first, "last:", last)
+    column = last
+    # print("column", column)
+    # print("trying to add column", column)
+    # print("appending ",row,column)
+    # if row not in rows:
+    rows.append(row)
+    # if column not in columns:
+    columns.append(column)
+    # print("row:", row)
+    # print("column:", column)
+    # print("line", line[:-1], "seatID:", row * 8 + column, "row", row, "column", column)
+    if (row * 8 + column) > maxID:
+        maxID = row * 8 + column
+# rows.sort()
+# columns.sort()
+print("rows:", rows, len(rows))
+print("columns:", columns, len(columns))
+print("max:", maxID)
+count = 0
+known = [None] * len(rows)
+cols = [None] * len(rows)
+for i in rows:
+    j = columns[count]
+    print("row", i, "column", j)
+    if i not in known:
+        known.append(i)
+        if cols[int(i)] == None:
+            cols[int(i)] = []
+        cols[int(i)].append(j)
+    count += 1
+print("known",known)
+print("cols",cols)
+# prev = 0
+# count = 0
+# for i in rows:
+#     if prev == 0:
+#         prev = i
+#     # print("prev", prev)
+#     # print("current", i)
+#     if prev != i:
+#         if count != 8:
+#             print("stopped at", count, "prev", prev, "i", i)
+#         prev = i
+#         count = 0
+#     count += 1
+# for i in range(1,128):
+#     for j in range(1,8):
+#         print(i, j)
+# The best algorithm: Brute Force
+# 66 * 8 + 6 INCORRECT
+# 65 * 8 + 6 = 526 INCORRECT
+# 65 * 8 + 7 = 527 INCORRECT
+# 65 * 8 + 5 = 525 INCORRECT
+# 65 * 8 + 4 = 524 CORRECT
+# 65 * 8 + 3 = 523 INCORRECT
+# 65 * 8 + 2 = 522 ?
+# 65 * 8 + 1 = 521 ?
+# 65 * 8 + 0 = 520 ?
